aafe7550def1b95f2718cc9924e93a0fb189add5
Teknix <kek@teknix.no>
2017-04-19 11:16:04 +0200

    Javascript: Dell.js

---
+++ javascript/Dell.js
@@ -0,0 +1,9 @@
+/* 
+
+Add the javascript below to a new bookmark in your browser.
+
+When clicking the bookmark it will show a prompt to enter the
+servicetag and then redirect to the dell website for that computer.
+
+*/
+javascript:const st=prompt("Enter Service Tag",""); window.location = "http://www.dell.com/support/home/no/no/nodhs1/product-support/servicetag/"+st+"/warranty"; void(0);
